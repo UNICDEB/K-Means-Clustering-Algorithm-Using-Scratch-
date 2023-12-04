066e97467597561c5b8d39b3ff73348c3370e3d2
UNICDEB <doloidebabrat99@gmail.com>
2023-12-04 15:20:37 +0530

    Added K_means.py

--- K_Means.py
+++ K_Means.py
@@ -11,13 +11,7 @@ def generate_anchors(annotations, num_clusters=9):
     Returns:
         anchor_boxes: List of anchor boxes in the form of [width, height].
     """
-    
-    # print("list Item :- ",annotations)
-    # Convert annotations to a numpy array
     annotations = np.array(annotations)
-    # print("After converting Array - \n", annotations)
-    # print(type(annotations))
-    
     # Perform KMeans clustering
     kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(annotations)
     anchor_boxes = kmeans.cluster_centers_
@@ -25,13 +19,7 @@ def generate_anchors(annotations, num_clusters=9):
     print("All Cluster Value = \n",anchor_boxes)
     
     return anchor_boxes
-# annotations=[[23,45],[34,78],[32,89],[13,25],[31,45],[32,78],[12,45],[29,90],[11,21],[31,45],[43,71],[32,78]]
-
-# annotations=[[23,45],[34,78],[32,89],[13,25],[31,45],[32,78],[12,45],[29,90],[11,21],[31,45],[43,71],[32,78]]
 csv_file = 'annotations_data.csv'
 data_array = np.genfromtxt(csv_file, delimiter=',')
-# print("Loaded data array:")
 annotations = data_array
-# print(data_array)
-# print(type(annotations))
 generate_anchors(annotations,num_clusters=9)
